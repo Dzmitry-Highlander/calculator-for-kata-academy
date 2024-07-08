a8d783bef0ebc3a9c697203e3db8492f6cf29080
Dzmitriy Krasiuk <strixmymail@gmail.com>
2024-07-08 23:53:39 +0300

    refactor Converter.java

--- src/main/java/service/impl/Converter.java
+++ src/main/java/service/impl/Converter.java
@@ -1,45 +1,36 @@
 package service.impl;
 
 import service.api.IConverter;
-import service.enums.EErrors;
 import service.enums.ENotations;
-import service.exception.ValidationException;
 
 import java.util.Objects;
 
 public class Converter implements IConverter {
-    private final String[] convertArray = new String[] {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X",
-            "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
-    String[] converted;
-    boolean existingFlag = false;
+    private final String[] convertArray = new String[] {
+            "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X",
+            "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"
+    };
 
     @Override
     public String[] convertInput(String[] input) {
-        if (Objects.equals(input[1], ENotations.ARABIC.getNotation())) {
-            converted = input[0].split("");
+        String[] converted = new String[input.length];
 
-            return converted;
-        } else if (Objects.equals(input[1], ENotations.ROMAN.getNotation())) {
-            converted = input[0].split("");
-
-            for (int i = 0; i < converted.length; i = i + 2) {
-                for (String roman : convertArray) {
-                    if (Objects.equals(roman, converted[i])) {
-                        existingFlag = true;
-
-                        break;
+        if (Objects.equals(input[3], ENotations.ARABIC.getNotation())) {
+            converted = input;
+        } else if (Objects.equals(input[3], ENotations.ROMAN.getNotation())) {
+            for (int i = 0; i < input.length - 2; i++) {
+                for (int j = 0; j < convertArray.length / 2; j++) {
+                    if (Objects.equals(input[i], convertArray[j])) {
+                        converted[i] = convertArray[j + 10];
                     }
-                }
-
-                if (existingFlag) {
-                    converted[i] = convertArray[i + 10];
                 }
             }
 
-            return converted;
-        } else {
-            throw new ValidationException(EErrors.InputError.getMessage());
+            converted[converted.length - 1] = input[input.length - 1];
+            converted[converted.length - 2] = input[input.length - 2];
         }
+
+        return converted;
     }
 
     @Override
