5ff74e067d3cca5dfff5ef6f8e590cb415afece4
Dzmitriy Krasiuk <strixmymail@gmail.com>
2024-07-09 02:44:52 +0300

    Refactor according test task

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,28 @@
 import java.util.Scanner;
 
+static int result;
+static Validator validator = new Validator();
+static Converter converter = new Converter();
+
 public static void main() {
     Scanner scanner = new Scanner(System.in);
     String input = scanner.nextLine();
 
-    System.out.println(Calculator.calc(input));
+    System.out.println(calc(input));
+}
+
+public static String calc(String input) {
+    String[] validated = validator.validation(input);
+    String[] converted = converter.convertInput(validated);
+
+    switch (converted[2]) {
+        case "+" -> result = Integer.parseInt(converted[0]) + Integer.parseInt(converted[1]);
+        case "-" -> result = Integer.parseInt(converted[0]) - Integer.parseInt(converted[1]);
+        case "*" -> result = Integer.parseInt(converted[0]) * Integer.parseInt(converted[1]);
+        case "/" -> result = Integer.parseInt(converted[0]) / Integer.parseInt(converted[1]);
+    }
+
+    return converter.convertOutput(result, converted[3]);
 }
 
 
