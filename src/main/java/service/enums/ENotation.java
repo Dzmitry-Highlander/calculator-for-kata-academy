d76e3f40c50160cbca91bdd7254b97a176e7c8e6
Dzmitriy Krasiuk <strixmymail@gmail.com>
2024-07-08 10:36:33 +0300

    Refactor ENotation.java

--- src/main/java/service/enums/ENotation.java
+++ src/main/java/service/enums/ENotation.java
@@ -1,6 +1,13 @@
 package service.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public enum ENotation {
-    ARABIC,
-    ROMAN
+    ARABIC ("Arabic"),
+    ROMAN ("Roman");
+
+    private final String notation;
 }
